5a947c1b615afc3144755bef7930380aab92c807
JhonathanNicolas <jhonathannicolas@gmail.com>
2020-11-15 17:39:55 -0300

    Add files via upload
    Drivers

---
+++ src/moveEngine.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <pigpio.h>
+ 
+#define PWMSTEP 512
+#define frequency_pwm 40 //[khz]
+#define LEFT 27
+#define RIGHT 25
+void move_pantilt(float moviment_axis)
+{
+ 
+
+
+    int duty_left, duty_right = 0;
+    duty_right =  (int)(moviment_axis)*(PWMSTEP) + PWMSTEP;  //seta o duty cycle do motor da direita
+    duty_left =   (int) (moviment_axis)*(PWMSTEP) + PWMSTEP; //seta o duty cycle do motor da esquerda
+
+
+   
+    gpioPWM(LEFT, 0); // configura o pino LEFT como saída PWM
+    gpioPWM(RIGHT, 0); // configura o pino RIGHT como saída PWM
+
+    gpioSetPWMfrequency(duty_right, frequency_pwm); 
+    gpioSetPWMfrequency(duty_left, frequency_pwm);
+  
+	           
+    }
+    return 0;
---
+++ src/sonar.c
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <wiringPi.h>
+#include "libSonar.h"
+
+using namespace std;
+
+#define TRIGGER 1
+#define ECHO 0
+
+int ultrassonic_distance()
+{
+    if (wiringPiSetup() == -1) //verifica se o pino pode ser usado
+        return -1;
+
+    Sonar sonar; //declara uma variável do tipo sonar
+    sonar.init(TRIGGER, ECHO); // inicializa os pinos
+    return sonar.distance(); // retorna a distância em cm
+}
